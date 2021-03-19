52dc15700b89d27ce54d1807f958fbc081e62d1c
rotarsebastian <rotar.seby1@gmail.com>
2021-03-19 15:37:34 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Be better at React
         </a>
       </header>
     </div>
